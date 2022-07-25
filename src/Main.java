942476d21f8b0ce096f03a21ed9d51becd6bbe06
Alexasc <923248@mail.ru>
2022-07-25 21:52:59 +0700

    homework6,

--- src/Main.java
+++ src/Main.java
@@ -10,42 +10,54 @@ public class Main {
             if (arrint[i] % 2 == 0) {
                 System.out.println(arrint[i]);
             } else System.out.println(arrint[i] + 1);
-
-
-
-
         }
 
-
-  //      {
-   //         if (i == arrint.length - 1) {
-   //             System.out.println(arrint[i]);
-  //          } else {
-  //              System.out.print(arrint[i]+ ", ");
-  //          }
-  //      }
-
-
-
-
-
-        double[] arr1 = new double[]{1.57, 7.654, 9.986};
-        for (int i = 0; i < arr1.length - 1; i++) System.out.print(arr1[i] + ", ");
-        System.out.println(arr1[arr1.length - 1]);
-
-
-        int[] arr2 = new int[]{1, 2, 3, 4};
-        int counter = arr2.length - 1;
+        for (int i = 0; i < arrint.length; i++) {
+            if (i == arrint.length - 1) {
+                System.out.println(arrint[i]);
+            } else {
+                System.out.print(arrint[i] + ", ");
+            }
+        }
+        int counter = arrint.length - 1;
         while (0 < counter) {
-            System.out.print(arr2[counter] + ", ");
+                        System.out.print(arrint[counter] + ", ");
             counter--;
+        }
+        System.out.println(arrint[0]);
 
+
+        double[] arr1 = new double[]{1.57, 7.654, 9.986};{
+        for (int i = 0; i < arr1.length - 1; i++) System.out.print(arr1[i] + ", ");
+        System.out.println(arr1[arr1.length - 1]);}
+
+        int couter = arr1.length - 1;
+        while (0 < couter) {
+            System.out.print(arr1[couter] + ", ");
+            couter--;
+        }
+        System.out.println(arr1[0]);
+
+        int[] arr2 = new int[]{11, 12, 13, 14};
+        int couter1 = arr2.length - 1;
+        while (0 < couter1){
+            System.out.print(arr2[couter1] + ", ");
+            couter1--;
+                    }
+        System.out.println(arr2[0]);
+        for (int i = 0; i < arr2.length - 1; i++){
+            System.out.print(arr2[i] + ", ");
+        }
+        System.out.print(arr2[arr2.length - 1]);
+
+
+
+      // Задание4
 
         }
-        System.out.print(arr2[0]);
+        }
 
-    }
-      }
+
 
 
 
